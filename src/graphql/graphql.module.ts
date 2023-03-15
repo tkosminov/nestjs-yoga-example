aa1cfc2692828b138bacd7632108900dd51be6b6
t.kosminov <t.kosminov@oblakogroup.ru>
2023-03-15 19:41:41 +0300

    gql rest api, gql logger, gql cache

--- src/graphql/graphql.module.ts
+++ src/graphql/graphql.module.ts
@@ -1,6 +1,13 @@
-import { GraphQLModule as NestJSGraphQLModule } from '@nestjs/graphql';
-import { Global, Module } from '@nestjs/common';
+import { GraphQLModule as NestJSGraphQLModule, GraphQLSchemaHost } from '@nestjs/graphql';
+import { Global, Inject, Module, OnModuleInit } from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
 import { YogaDriverConfig, YogaDriver } from '@graphql-yoga/nestjs';
+
+import { Application } from 'express';
+import { OpenAPI, useSofa } from 'sofa-api';
+import { serve, setup } from 'swagger-ui-express';
+
+import { LoggerStore } from '../logger/logger.store';
 
 import { GraphQLOptions } from './graphql.options';
 
@@ -17,4 +24,45 @@ import { GraphQLOptions } from './graphql.options';
   providers: [],
   exports: [],
 })
-export class GraphQLModule {}
+export class GraphQLModule implements OnModuleInit {
+  constructor(
+    @Inject(GraphQLSchemaHost) private readonly schemaHost: GraphQLSchemaHost,
+    @Inject(HttpAdapterHost) private readonly httpAdapterHost: HttpAdapterHost
+  ) {}
+
+  onModuleInit(): void {
+    const { schema } = this.schemaHost;
+    const { httpAdapter } = this.httpAdapterHost;
+
+    const app: Application = httpAdapter.getInstance();
+
+    const open_api = OpenAPI({
+      schema,
+      info: {
+        title: 'NestJS-Yoga REST API',
+        version: '1.0.0',
+      },
+    });
+
+    app.use(
+      '/graphql-rest',
+      useSofa({
+        schema,
+        basePath: '/graphql-rest',
+        depthLimit: 0,
+        onRoute(info) {
+          open_api.addRoute(info, {
+            basePath: '/graphql-rest',
+          });
+        },
+        context: ({ req }: { req: Request & { logger_store: LoggerStore } }) => ({
+          req,
+          logger_store: req.logger_store,
+        }),
+      })
+    );
+
+    const open_api_definitions = open_api.get();
+    app.use('/api/graphql-rest', serve, setup(open_api_definitions));
+  }
+}
--- src/graphql/graphql.options.ts
+++ src/graphql/graphql.options.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { GqlOptionsFactory } from '@nestjs/graphql';
 import { YogaDriver, YogaDriverConfig } from '@graphql-yoga/nestjs';
+import { useResponseCache } from '@envelop/response-cache';
+import { useLogger } from '@envelop/core';
 
-import { GraphQLSchema } from 'graphql';
+import { DocumentNode, GraphQLArgs, GraphQLSchema } from 'graphql';
 import { Request } from 'express';
 import { DataSource } from 'typeorm';
 import { setDataSource } from 'nestjs-graphql-easy';
@@ -19,14 +21,62 @@ export class GraphQLOptions implements GqlOptionsFactory {
     return {
       graphiql: true,
       driver: YogaDriver,
-      autoSchemaFile: __dirname + '/schema.graphql',
+      autoSchemaFile: true,
       context: ({ req }: { req: Request & { logger_store: LoggerStore } }) => ({
         req,
         logger_store: req.logger_store,
+        current_user: null,
       }),
       transformSchema: (schema: GraphQLSchema) => {
         return schema;
       },
+      plugins: [
+        useResponseCache({
+          session: ({ current_user }: { current_user: unknown }) => String(current_user),
+          ttl: process.env.CACHE_TTL ? +process.env.CACHE_TTL : 5_000,
+          invalidateViaMutation: true,
+        }),
+        useLogger({
+          logFn: (
+            event_name: string,
+            {
+              args,
+            }: {
+              args: GraphQLArgs & {
+                document: DocumentNode;
+                contextValue: {
+                  req: Request;
+                  logger_store: LoggerStore;
+                  params: {
+                    query: string;
+                  };
+                };
+              };
+              result?: unknown;
+            }
+          ) => {
+            const ctx = args.contextValue;
+            const logger_store: LoggerStore = ctx.logger_store;
+
+            let operation: string;
+            const selections: string[] = [];
+
+            args.document.definitions.forEach((definition) => {
+              if (definition.kind === 'OperationDefinition') {
+                operation = definition.operation;
+
+                definition.selectionSet.selections.forEach((selection) => {
+                  if (selection.kind === 'Field') {
+                    selections.push(selection.name.value);
+                  }
+                });
+              }
+            });
+
+            logger_store.info(`GraphQL ${event_name}`, { event: event_name, operation, selections });
+          },
+        }),
+      ],
     };
   }
 }
